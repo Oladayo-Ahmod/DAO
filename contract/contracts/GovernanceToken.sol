0994fb1dd8995cee8bf64ad0d88e11afc22d5093
oladayo ahmod <oladayoahmod112@gmail.com>
2023-04-28 20:51:46 -0700

    set erc20 erc20permit constructors

--- contract/contracts/GovernanceToken.sol
+++ contract/contracts/GovernanceToken.sol
@@ -4,5 +4,15 @@ import "@openzeppelin/contracts/token/ERC20/Extensions/ERC20Votes.sol";
 
 contract GovernanceToken is ERC20Votes {
     
-    uint256 constant MAX_TOKEN_PER_USER = 1000;
+    uint256 constant MAX_TOKEN_PER_USER = 1000; //max token / user
+    uint256 constant TOTAL_SUPPLY = 1000000 *10**18; // total supply to 18 decimals
+
+    mapping (address => bool) s_hasClaimedToken; // check if token is claimed
+    address[] public s_holders;
+
+    constructor(uint256 _keepPercentage){
+        ERC20("AhmodToken","AM");
+        ERC20Permit("AhmodToken");
+    }
+
 }
