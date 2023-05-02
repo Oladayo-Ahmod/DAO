9207b063020ef0b9a7aa7debc5141aecc172e546
oladayo ahmod <oladayoahmod112@gmail.com>
2023-05-01 23:27:10 -0700

    test Box : stores and retrieves value

--- contract/test/script.test.js
+++ contract/test/script.test.js
@@ -1,34 +1,56 @@
 const {assert,expect} = require('chai')
 const {ethers,network} = require('hardhat')
 
-describe("DAO", async ()=>{
+describe("DAO", ()=>{
     let GOVERNORTOKEN,DAOGOVERNOR,TIMELOCK,BOX
 
-    beforeEach(async()=>{
-        let provider = await ethers.getContractFactory("GovernanceToken")
-        GOVERNORTOKEN = await provider.deploy('10')
+    describe("governance token", ()=>{
+
+        beforeEach(async()=>{
+            let provider = await ethers.getContractFactory("GovernanceToken")
+            GOVERNORTOKEN = await provider.deploy('10')
+        })
+
+        it("deploys governance token", async()=>{
+            const address = await GOVERNORTOKEN.deployed()
+            assert.notEqual(address,0x0)
+            assert.notEqual(address,undefined)
+            assert.notEqual(address,null)
+            assert.notEqual(address,'')
+        })
+        it("claims token", async ()=>{
+            const user = await ethers.getSigner(1)
+            await GOVERNORTOKEN.connect(user).claimToken()
+            const balance = await GOVERNORTOKEN.balanceOf(user.address)
+            assert.equal(balance.toString(),ethers.utils.parseEther('1000'))
+        })
+        it("gets total holder", async()=>{
+            await GOVERNORTOKEN.deployed()
+            const [,user1,user2,user3] = await ethers.getSigners()
+            await GOVERNORTOKEN.connect(user1).claimToken()
+            await GOVERNORTOKEN.connect(user2).claimToken()
+            await GOVERNORTOKEN.connect(user3).claimToken()
+            const holders = await GOVERNORTOKEN.getTokenHolders()
+            assert.equal(holders.toString(),'4')
+        })
     })
 
-    it("deploys governance token", async()=>{
-        const address = await GOVERNORTOKEN.deployed()
-        assert.notEqual(address,0x0)
-        assert.notEqual(address,undefined)
-        assert.notEqual(address,null)
-        assert.notEqual(address,'')
+    describe("Box", ()=>{
+
+        beforeEach(async()=>{
+            let provider = await ethers.getContractFactory("Box")
+            BOX = await provider.deploy('10')
+        })
+
+        it("only owner stores", async()=>{
+            const owner = await BOX.deployed()
+            const store = await BOX.store(10)
+            const retrieve = await BOX.retrieve()
+            assert.equal(retrieve.toString(),'10')
+
+        })
     })
-    it("claims token", async ()=>{
-        const user = await ethers.getSigner(1)
-        await GOVERNORTOKEN.connect(user).claimToken()
-        const balance = await GOVERNORTOKEN.balanceOf(user.address)
-        assert.equal(balance.toString(),ethers.utils.parseEther('1000'))
-    })
-    it("gets total holder", async()=>{
-        await GOVERNORTOKEN.deployed()
-        const [,user1,user2,user3] = await ethers.getSigners()
-        await GOVERNORTOKEN.connect(user1).claimToken()
-        await GOVERNORTOKEN.connect(user2).claimToken()
-        await GOVERNORTOKEN.connect(user3).claimToken()
-        const holders = await GOVERNORTOKEN.getTokenHolders()
-        assert.equal(holders.toString(),'4')
-    })
+
+
+    
 })
